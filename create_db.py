86b22193dd8f2ff164bbe6f487147c6343119f07
Kanchan9m <kanchanmehta1232@gmail.com>
2023-09-30 12:52:30 +0530

    'flask'

--- create_db.py
+++ create_db.py
@@ -1,11 +1,11 @@
-import mysql.connector
-mydb = mysql.connector.connect(
+import mysql.connector as mysqlconnector
+mydb = mysqlconnector.connect(
     host="localhost",
     user = "root",
-    passwd = "password123",
+    password = "1234"
 )
 my_cursor = mydb.cursor()
-# my_cursor.execute("CREATE DATABASE our_users")
+my_cursor.execute("CREATE DATABASE users")
 
 my_cursor.execute("SHOW DATABASES")
 for db in my_cursor:
--- hello.py
+++ hello.py
@@ -1,27 +1,30 @@
-import email
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+
+from flask_migrate import Migrate
 from datetime import datetime
 
 app = Flask(__name__)
-#add datebase
+# add datebase
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password123@localhost/our_users'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost:3306/users'
 app.config['SECRET_KEY'] = "my super key that no one supposed to know"
 # app = Flask(__name__, template_folder='templatesaaa')
 
 db = SQLAlchemy(app)
-app.app_context().push()
+migrate = Migrate(app, db)
+# app.app_context().push()
 
 # create model
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
+    fovorite_color = db.Column(db.String(100))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
     #Create a string
     def __repr__(self):
@@ -33,7 +36,34 @@ class Users(db.Model):
 class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
+    favorite_color = StringField("Favorite Color")
     submit = SubmitField("Submit")
+
+#update
+@app.route('/update/<int:id>', methods=['GET', "POST"])
+def update(id):
+    form = UserForm()
+    name_to_update = Users.query.get_or_404(id)
+    if request.method == 'POST':
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        name_to_update.favorite_color = request.form['favorite_color']
+        try:
+            db.session.commit()
+            flash("User Updated Succesfully!")
+            return render_template("update.html",
+            form=form,
+            name_to_update = name_to_update)
+        except:
+            flash("Error Try again..")
+            return render_template("update.html",
+            form=form,
+            name_to_update = name_to_update)
+    else:
+        return render_template("update.html",
+        form=form,
+        name_to_update = name_to_update)
+
 #Create a form class
 class NamerForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
@@ -53,12 +83,13 @@ def add_user():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = Users(name=form.name.data, email=form.email.data)
+            user = Users(name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
         form.name.data = ''
         form.email.data = ''
+        form.favorite_color.data = ''
         flash("User added seccessfully")
     our_users= Users.query.order_by(Users.date_added)
     return render_template("add_user.html",
